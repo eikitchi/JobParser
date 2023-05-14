e4703fe16032fa5dab7e2e9c9b3ff737e1c1a066
davyd <davydow.val@yandex.ru>
2023-05-14 22:30:57 +0700

    A class for obtaining vacancies with hh.ru via the site API

---
+++ hh_api.py
@@ -0,0 +1,22 @@
+from abstract_api import AbstractVacanciesAPI
+
+import requests
+
+
+class HeadHunterAPI(AbstractVacanciesAPI):
+    """
+    Класс для получения вакансий с hh.ru через API сайта
+    """
+
+    def __init__(self):
+        self.url = "https://api.hh.ru/vacancies"
+
+    def get_vacancies(self, text):
+        params = {
+            "text": text,
+            "area": 113,
+            "per_page": 100
+        }
+        response = requests.get(self.url, params=params)
+        vacancies = response.json()
+        return vacancies["items"]
